909f0257d10c0a3379db02efc04f527a113b06f6
neritan <neritan.milaqi@gmail.com>
2025-01-23 12:49:48 +0100

    commit

--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-require('./src/config/mongoose.config');
-
-var app = express();
-
-const port = 3080;
-
-app.listen( port, () => console.log(`Listening on port: ${port}`) );
--- server/src/config/mongoose.config.js
+++
@@ -1,5 +0,0 @@
-const mongoose  = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/mern")
-.then(() => console.log("Connection Mongo Established!!"))
-.catch(err => console.log("Connection Mongo Error", err));
